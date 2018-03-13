329a34e17097cf6f6ddfcb2bb7a79843b317974c
Maya Ramamurthy <ramamums@mcmaster.ca>
2018-03-13 16:04:59 -0400

    First commit

---
+++ src/bytecodegenerator.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class bytecodegenerator {
+
+	public static void main(String[] args) {
+		File file = new File("HelloWorld.java");
+		 try {
+
+		        Scanner sc = new Scanner(file);
+
+		        while (sc.hasNextLine()) {
+		            int i = sc.nextInt();
+		            System.out.println(i);
+		        }
+		        sc.close();
+		    } 
+		    catch (FileNotFoundException e) {
+		        e.printStackTrace();
+		    }
+
+	}
+
+}
